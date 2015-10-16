9947a9b8362307fa8ce7bb1e7ceea9648a863b6f
Harold Gottschalk <hgottschalk@softstackfactory.com>
2015-10-16 23:43:05 +0000

    passport point

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -7,23 +7,78 @@ app.config([
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
-            controller: 'MainCtrl'
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['posts', function(posts){
+                  return posts.getAll();
+                }]
+            }
         })
         .state('posts', {
           url: '/posts/{id}',
           templateUrl: '/posts.html',
-          controller: 'PostsCtrl'
+          controller: 'PostsCtrl',
+          resolve: {
+            post: ['$stateParams', 'posts', function($stateParams, posts) {
+              return posts.get($stateParams.id);
+            }]
+          }
         });
         
         $urlRouterProvider.otherwise('home');
     }]);
 
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http',
+function($http){
     var o = {
         posts : []
     }
+    
+    //gets all the post
+    o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+          angular.copy(data, o.posts);
+        });
+    };
+
+    //Create a post
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data){
+        o.posts.push(data);
+      });
+    };    
+
+    //Upvote a post
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post._id + '/upvote')
+        .success(function(data){
+          post.upvotes += 1;
+        });
+    };  
+    
+    //Get a individual post
+    o.get = function(id) {
+      return $http.get('/posts/' + id).then(function(res){
+        return res.data;
+      });
+    };
+    
+    // Add a comment
+    o.addComment = function(id, comment) {
+      return $http.post('/posts/' + id + '/comments', comment);
+    };
+    
+    //Upvote a comment
+    o.upvoteComment = function(post, comment) {
+      return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+        .success(function(data){
+          comment.upvotes += 1;
+        });
+    };
+    
     return o;
+   
 }]);
 
 app.controller('MainCtrl', [
@@ -32,38 +87,39 @@ function($scope, posts){
     $scope.posts = posts.posts;
     $scope.addPost = function(){
         if(!$scope.title || $scope.title === ''){ return};
-        $scope.posts.push(
-            {title: $scope.title,
+        posts.create({
+            title: $scope.title,
             link: $scope.link,
-            upvotes: 0,
-            comments: [
-                {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-              ]
-            });
+        });
+
         $scope.title = '';
         $scope.link = '';
     };
+    // increment upvotes on post
     $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
+        posts.upvote(post);
     };
 }]);
 
 app.controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-    
-    $scope.post = posts.posts[$stateParams.id];
-    
+'post',
+function($scope, posts, post){
+    $scope.post = post;    
     $scope.addComment = function(){
       if($scope.body === '') { return; }
-      $scope.post.comments.push({
+      posts.addComment(post._id, {
         body: $scope.body,
         author: 'user',
-        upvotes: 0
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
       });
       $scope.body = '';
+    };
+    
+    // increment upvote for a comment
+    $scope.incrementUpvotes = function(comment){
+      posts.upvoteComment(post, comment);
     };
 }]);
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,101 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+// get all post from the database
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+// Save a post to the database
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+// Preloading a post by post id
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+// Preloading a comment by post id and comment Id
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
+// creates a route to grab post by id
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+//Update the post upvote count
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+//Saves a comment to a post
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+
+//Update the a post comments upvote count
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
 
 module.exports = router;
+
+
